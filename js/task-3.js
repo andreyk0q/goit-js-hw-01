a5be16206e39a50ebf33245f0672f4797e12a5b7
Andrii Dovgopoluk <andrijdovgopoluk@gmail.com>
2025-12-23 10:04:46 +0200

    fixed tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,12 @@
-function getElementWidth (content, padding, border) {
-    const contentNum = parseFloat(content);
-    const paddingNum = parseFloat(padding);
-    const borderNum = parseFloat(border);
+function getElementWidth(content, padding, border) {
+  const contentNum = parseFloat(content);
+  const paddingNum = parseFloat(padding);
+  const borderNum = parseFloat(border);
 
-    return totalWidth;
+  const totalWidth = contentNum + 2 * paddingNum + 2 * borderNum;
+  return totalWidth;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth("50px", "8px", "4px"));     // 74
+console.log(getElementWidth("60px", "12px", "8.5px"));  // 101
+console.log(getElementWidth("200px", "0px", "0px"));    // 200
